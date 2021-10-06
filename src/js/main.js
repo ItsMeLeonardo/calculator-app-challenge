c6cdfd834226cdef223a61807701f5bfed169cde
Carlos Leonardao <74982565+ItsMeLeonardo@users.noreply.github.com>
2021-10-06 17:06:47 -0500

    complete the themes with JS and CSS

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,12 @@
 
 const position = ['.15rem', '1.3rem', '3rem']
-const themes = document.querySelectorAll('.calc__label')
+const classThemes = ['', 'theme-light', 'theme-purple']
+const themes = document.querySelectorAll('.calc__label'),
+    body = document.querySelector('body')
 
 themes.forEach((theme, index) => {
     theme.addEventListener('click', () => {
         document.documentElement.style.setProperty('--theme-position', position[index])
+        body.setAttribute('class', classThemes[index])
     })
 })
